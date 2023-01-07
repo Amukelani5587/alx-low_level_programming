de69333171ce91b63d820fd857059aa7251df18f
Amukelani <111043334+Amukelani5587@users.noreply.github.com>
2023-01-08 01:25:53 +0200

    1-main.c

---
+++ 0x1A-hash_tables/1-main.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    char *s;
+
+    s = "cisfun";
+    printf("%lu\n", hash_djb2((unsigned char *)s));
+    s = "Don't forget to tweet today";
+    printf("%lu\n", hash_djb2((unsigned char *)s));
+    s = "98";
+    printf("%lu\n", hash_djb2((unsigned char *)s));
+    return (EXIT_SUCCESS);
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
